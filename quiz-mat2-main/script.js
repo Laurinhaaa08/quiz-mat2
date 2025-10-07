65ec06ade963d4fbd7bdbe2567cc0a6b35dd2f84
Laurinhaaa08 <161832000+Laurinhaaa08@users.noreply.github.com>
2025-10-07 07:48:12 -0300

    Add files via upload

---
+++ quiz-mat2-main/script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: "qual é o maior animal do mundo?"
+        answers: [
+            { id:1, text: "tubarão", correct: false}
+            { id:2, text: "tubarão", correct: false}
+            { id:3, text: "tubarão", correct: false}
+            { id:4, text: "tubarão", correct: false}
+        ]
+    }
+]
